cad55f0c7f7245b0c4910a0a2f47d67f094de6e8
AlexGarSer <alex.garciase@uanl.edu.mx>
2022-07-12 13:37:10 -0500

    cambio de PORT a port en index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import app from './app'
 
 //Se extrae el puerto 
-app.listen(app.get('PORT'))
+app.listen(app.get('port'))
 
 // Se imprime el puerto del servidor
-console.log('Server on port',app.get('PORT'));
+console.log('Server on port',app.get('port'));
 
